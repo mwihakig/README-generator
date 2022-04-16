dcbf8ea6a40ef4becf903a11b83ab75aa0dd2739
mwihakig <97566064+mwihakig@users.noreply.github.com>
2022-04-16 15:58:17 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hi');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+  };
+  
+  printProfileData(profileDataArgs);
